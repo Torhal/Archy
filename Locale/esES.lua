f0ae31638cbd3e137a8baa776a3c4331943e66b8
James D. Callahan III <darkenelf@gmail.com>
2011-06-07 19:16:26 -0500

    Fixed all locale files to use the localization app and split Spanish.lua into esES.lua and esMX.lua

---
+++ Locale/esES.lua
@@ -0,0 +1,4 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("Archy", "esES")
+if not L then return end
+
+--@localization(locale="esES", format="lua_additive_table", handle-unlocalized="ignore", escape-non-ascii=false, same-key-is-true=true)@
