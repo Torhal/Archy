c6b353c490d03ec56b3572b79be544044de8c397
Ethan Centaurai <ethancentaurai@outlook.com>
2014-11-17 18:39:28 +0000

    Added the Ruins of the First Bastion Digsite in Gorgrond.

--- Digsites.lua
+++ Digsites.lua
@@ -1732,6 +1732,12 @@ local DIG_SITES = {
 		blob_id = 308011,
 		race = DIG_SITE_RACES.DRAENOR_CLANS,
 	},
+	["Ruins of the First Bastion Digsite"] = {
+		continent = 7,
+		map = 949,
+		blob_id = 308015,
+		race = DIG_SITE_RACES.DRAENOR_CLANS,
+	},
 	["Southwind Cliffs Digsite"] = {
 		continent = 7,
 		map = 941,
