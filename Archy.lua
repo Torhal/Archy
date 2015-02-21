f7e9fd9ec443a5c8ac18936298d63f7c4ba25217
James D. Callahan III <jcallahan@curse.com>
2015-02-21 16:33:16 -0600

    PROFILE_DEFAULTS => private.DEFAULT_SETTINGS, and moved it to Constants.lua

--- Archy.lua
+++ Archy.lua
@@ -52,214 +52,6 @@ private.MAX_ARCHAEOLOGY_RANK = MAX_ARCHAEOLOGY_RANK
 local MAP_ID_TO_ZONE_ID = {} -- Popupated in Archy:OnInitialize()
 local MAP_ID_TO_ZONE_NAME = {} -- Popupated in Archy:OnInitialize()
 
-local PROFILE_DEFAULTS = {
-	profile = {
-		general = {
-			enabled = true,
-			show = true,
-			stealthMode = false,
-			combathide = true,
-			icon = {
-				hide = false
-			},
-			locked = false,
-			confirmSolve = true,
-			showSkillBar = true,
-			sinkOptions = {
-				sink20OutputSink = "LibToast-1.0",
-			},
-			easyCast = false,
-			autoLoot = true,
-			theme = "Graphical",
-			manualTrack = false,
-		},
-		artifact = {
-			show = true,
-			position = {
-				"TOPRIGHT",
-				"TOPRIGHT",
-				-400,
-				-425
-			},
-			anchor = "TOPRIGHT",
-			positionX = 100,
-			positionY = -300,
-			scale = 0.75,
-			alpha = 1,
-			filter = true,
-			announce = true,
-			keystoneAnnounce = true,
-			ping = true,
-			keystonePing = true,
-			blacklist = {},
-			autofill = {},
-			style = "Compact",
-			borderAlpha = 1,
-			bgAlpha = 0.5,
-			font = {
-				name = "Friz Quadrata TT",
-				size = 14,
-				shadow = true,
-				outline = "",
-				color = {
-					r = 1,
-					g = 1,
-					b = 1,
-					a = 1
-				}
-			},
-			fragmentFont = {
-				name = "Friz Quadrata TT",
-				size = 14,
-				shadow = true,
-				outline = "",
-				color = {
-					r = 1,
-					g = 1,
-					b = 1,
-					a = 1
-				}
-			},
-			keystoneFont = {
-				name = "Friz Quadrata TT",
-				size = 12,
-				shadow = true,
-				outline = "",
-				color = {
-					r = 1,
-					g = 1,
-					b = 1,
-					a = 1
-				}
-			},
-			fragmentBarColors = {
-				["Normal"] = {
-					r = 1,
-					g = 0.5,
-					b = 0
-				},
-				["Solvable"] = {
-					r = 0,
-					g = 1,
-					b = 0
-				},
-				["Rare"] = {
-					r = 0,
-					g = 0.4,
-					b = 0.8
-				},
-				["AttachToSolve"] = {
-					r = 1,
-					g = 1,
-					b = 0
-				},
-				["FirstTime"] = {
-					r = 1,
-					g = 1,
-					b = 1
-				},
-			},
-			fragmentBarTexture = "Blizzard Parchment",
-			borderTexture = "Blizzard Dialog Gold",
-			backgroundTexture = "Blizzard Parchment",
-		},
-		digsite = {
-			show = true,
-			position = {
-				"TOPRIGHT", "TOPRIGHT", -400, -200
-			},
-			anchor = "TOPRIGHT",
-			positionX = 400,
-			positionY = -300,
-			scale = 0.75,
-			alpha = 1,
-			style = "Extended",
-			sortByDistance = true,
-			announceNearest = true,
-			distanceIndicator = {
-				enabled = true,
-				green = 40,
-				yellow = 80,
-				position = {
-					"CENTER",
-					"CENTER",
-					0,
-					0
-				},
-				anchor = "TOPLEFT",
-				undocked = false,
-				showSurveyButton = true,
-				showCrateButton = true,
-				showLorItemButton = true,
-				font = {
-					name = "Friz Quadrata TT",
-					size = 16,
-					shadow = false,
-					outline = "OUTLINE",
-					color = {
-						r = 1,
-						g = 1,
-						b = 1,
-						a = 1
-					}
-				},
-			},
-			borderAlpha = 1,
-			bgAlpha = 0.5,
-			font = {
-				name = "Friz Quadrata TT",
-				size = 18,
-				shadow = true,
-				outline = "",
-				color = {
-					r = 1,
-					g = 1,
-					b = 1,
-					a = 1
-				}
-			},
-			zoneFont = {
-				name = "Friz Quadrata TT",
-				size = 14,
-				shadow = true,
-				outline = "",
-				color = {
-					r = 1,
-					g = 0.82,
-					b = 0,
-					a = 1
-				}
-			},
-			minimal = {
-				showDistance = false,
-				showZone = false,
-				showDigCounter = true,
-			},
-			borderTexture = "Blizzard Dialog Gold",
-			backgroundTexture = "Blizzard Parchment",
-		},
-		minimap = {
-			show = true,
-			nearest = true,
-			fragmentNodes = true,
-			fragmentIcon = "CyanDot",
-			fragmentColorBySurveyDistance = true,
-			useBlobDistance = true,
-		},
-		tooltip = {
-			filter_continent = true,
-			hideDelay = 0.1,
-			scale = 1,
-		},
-		tomtom = {
-			crazyArrowEnabled = true,
-			distance = 125,
-			enabled = true,
-			ping = true,
-		},
-	},
-}
-
 local GLOBAL_COOLDOWN_TIME = 1.5
 local SECURE_ACTION_BUTTON -- Populated in Archy:OnInitialize()
 local SURVEY_SPELL_ID = 80451
@@ -502,13 +294,18 @@ end
 private.IsTaintable = IsTaintable
 
 function private:ResetPositions()
-	self.db.digsite.distanceIndicator.position = { unpack(PROFILE_DEFAULTS.profile.digsite.distanceIndicator.position) }
-	self.db.digsite.distanceIndicator.anchor = PROFILE_DEFAULTS.profile.digsite.distanceIndicator.anchor
-	self.db.digsite.distanceIndicator.undocked = PROFILE_DEFAULTS.profile.digsite.distanceIndicator.undocked
-	self.db.digsite.position = { unpack(PROFILE_DEFAULTS.profile.digsite.position) }
-	self.db.digsite.anchor = PROFILE_DEFAULTS.profile.digsite.anchor
-	self.db.artifact.position = { unpack(PROFILE_DEFAULTS.profile.artifact.position) }
-	self.db.artifact.anchor = PROFILE_DEFAULTS.profile.artifact.anchor
+	local defaultSettings = private.DEFAULT_SETTINGS.profile
+	local settings = Archy.db.profile
+
+	settings.digsite.distanceIndicator.position = { unpack(defaultSettings.digsite.distanceIndicator.position) }
+	settings.digsite.distanceIndicator.anchor = defaultSettings.digsite.distanceIndicator.anchor
+	settings.digsite.distanceIndicator.undocked = defaultSettings.digsite.distanceIndicator.undocked
+	settings.digsite.position = { unpack(defaultSettings.digsite.position) }
+	settings.digsite.anchor = defaultSettings.digsite.anchor
+
+	settings.artifact.position = { unpack(defaultSettings.artifact.position) }
+	settings.artifact.anchor = defaultSettings.artifact.anchor
+
 	Archy:ConfigUpdated()
 	Archy:UpdateFramePositions()
 end
@@ -875,7 +672,7 @@ end
 function Archy:OnInitialize()
 	private.isLoading = true
 
-	self.db = LibStub("AceDB-3.0"):New("ArchyDB", PROFILE_DEFAULTS, 'Default')
+	self.db = LibStub("AceDB-3.0"):New("ArchyDB", private.DEFAULT_SETTINGS, 'Default')
 	self.db.RegisterCallback(self, "OnNewProfile", "OnProfileUpdate")
 	self.db.RegisterCallback(self, "OnProfileChanged", "OnProfileUpdate")
 	self.db.RegisterCallback(self, "OnProfileCopied", "OnProfileUpdate")
@@ -915,7 +712,7 @@ function Archy:OnInitialize()
 	self.db.char.digsites.blacklist = self.db.char.digsites.blacklist or {}
 
 	private.db = self.db.profile
-	prevTheme = private.db and private.db.general and private.db.general.theme or PROFILE_DEFAULTS.profile.general.theme
+	prevTheme = private.db and private.db.general and private.db.general.theme or private.DEFAULT_SETTINGS.profile.general.theme
 
 	LDBI:Register("Archy", private.LDB_object, private.db.general.icon)
 
@@ -1136,7 +933,7 @@ function Archy:OnProfileUpdate(event, database, ProfileKey)
 	local newTheme
 	if database then
 		if event == "OnProfileChanged" or event == "OnProfileCopied" then
-			newTheme = database.profile and database.profile.general and database.profile.general.theme or PROFILE_DEFAULTS.profile.general.theme
+			newTheme = database.profile and database.profile.general and database.profile.general.theme or private.DEFAULT_SETTINGS.profile.general.theme
 		elseif event == "OnProfileReset" or event == "OnNewProfile" then
 			newTheme = database.defaults and database.defaults.profile and database.defaults.profile.general and database.defaults.profile.general.theme
 		end
